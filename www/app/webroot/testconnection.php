9f607bb990784e7d9c62589add346148ad2d1de1
David Baker <dbaker@acorncomputersolutions.com>
2014-03-19 11:01:47 -0600

    Removed test files from our webroot, we won't be using these

--- www/app/webroot/testconnection.php
+++
@@ -1,8 +0,0 @@
-<?php $link = mysql_connect('phelixinfosolution.com','phelixin_track','track123#');
-    if (!$link)
-    {
-        die('Could not connect: ' . mysql_error());
-    }
-    echo 'Connected successfully';
-    mysql_close($link);
-    ?> 
