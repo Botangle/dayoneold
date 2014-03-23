5bf728ea5f732d3b87db179b4c311bd598158354
Ben Sima <bensima@gmail.com>
2014-03-23 17:41:36 -0400

    code formatting - better readibility

--- www/app/Plugin/Users/Config/routes.php
+++ www/app/Plugin/Users/Config/routes.php
@@ -1,36 +1,23 @@
 <?php
 
 // Users
-CroogoRouter::connect('/registration/:type', array('plugin' => 'users', 'controller' => 'users', 'action' => 'registration'), array('pass' => array('type'))); 
+CroogoRouter::connect('/registration/:type', array('plugin' => 'users', 'controller' => 'users', 'action' => 'registration'), array('pass' => array('type')));
 CroogoRouter::connect('/register', array('plugin' => 'users', 'controller' => 'users', 'action' => 'add'));
 CroogoRouter::connect('/login', array('plugin' => 'users', 'controller' => 'users', 'action' => 'login'));
 CroogoRouter::connect('/logout', array('plugin' => 'users', 'controller' => 'users', 'action' => 'logout'));
 CroogoRouter::connect('/users/accountsetting', array('plugin' => 'users', 'controller' => 'users', 'action' => 'accountsetting'));
-
 CroogoRouter::connect('/user/search', array('plugin' => 'users', 'controller' => 'users', 'action' => 'search'));
-
-CroogoRouter::connect('/user/search/:online', array('plugin' => 'users', 'controller' => 'users', 'action' => 'search'), array('pass' => array('online')));	
-CroogoRouter::connect('/users/billing', array('plugin' => 'users', 'controller' => 'users', 'action' => 'billing'));
-
+CroogoRouter::connect('/user/search/:online', array('plugin' => 'users', 'controller' => 'users', 'action' => 'search'), array('pass' => array('online')));
 CroogoRouter::connect('/users/lessons', array('plugin' => 'users', 'controller' => 'users', 'action' => 'lessons'));
-
 CroogoRouter::connect('/users/createlessons', array('plugin' => 'users', 'controller' => 'users', 'action' => 'lessons_add'));
-
 CroogoRouter::connect('/users/users/createlessons', array('plugin' => 'users', 'controller' => 'users', 'action' => 'lessons_add'));
-
 CroogoRouter::connect('/users/createlessons/:type/:tutorname', array('plugin' => 'users', 'controller' => 'users', 'action' => 'lessons_add'),array('pass'=>array('type','tutorname')));
-
-CroogoRouter::connect('/users/searchstudent', array('plugin' => 'users', 'controller' => 'users', 'action' => 'searchstudent')); 
-
-CroogoRouter::connect('/users/updateremaining/', array('plugin' => 'users', 'controller' => 'users', 'action' => 'updateremaining')); 
-CroogoRouter::connect('/users/paymentmade/', array('plugin' => 'users', 'controller' => 'users', 'action' => 'paymentmade')); 
-
+CroogoRouter::connect('/users/searchstudent', array('plugin' => 'users', 'controller' => 'users', 'action' => 'searchstudent'));
+CroogoRouter::connect('/users/updateremaining/', array('plugin' => 'users', 'controller' => 'users', 'action' => 'updateremaining'));
+CroogoRouter::connect('/users/paymentmade/', array('plugin' => 'users', 'controller' => 'users', 'action' => 'paymentmade'));
 CroogoRouter::connect('/users/changelesson/:lessonid', array('plugin' => 'users', 'controller' => 'users', 'action' => 'changelesson'),array('pass'=>array('lessonid')));
 CroogoRouter::connect('/users/lessonreviews/:lessonid', array('plugin' => 'users', 'controller' => 'users', 'action' => 'lessonreviews'),array('pass'=>array('lessonid')));
-
-
 CroogoRouter::connect('/users/confirmedbytutor/:lessonid', array('plugin' => 'users', 'controller' => 'users', 'action' => 'confirmedbytutor'),array('pass'=>array('lessonid')));
-
 CroogoRouter::connect('/users/mycalander', array('plugin' => 'users', 'controller' => 'users', 'action' => 'mycalander'));
 CroogoRouter::connect('/users/calandarevents', array('plugin' => 'users', 'controller' => 'users', 'action' => 'calandarevents'));
 CroogoRouter::connect('/users/whiteboarddata/:lessonid', array('plugin' => 'users', 'controller' => 'users', 'action' => 'whiteboarddata'),array('pass'=>array('lessonid')));
@@ -40,28 +27,56 @@ CroogoRouter::connect('/passwordrecovery', array('plugin' => 'users', 'controlle
 CroogoRouter::connect('/user/:username', array(
 	'plugin' => 'users', 'controller' => 'users', 'action' => 'view'), array('pass' => array('username')
 ));
-
 CroogoRouter::connect('/users/topchart', array(
-	'plugin' => 'users', 'controller' => 'users', 'action' => 'topchart'));
+	'plugin' => 'users',
+	'controller' => 'users',
+	'action' => 'topchart')
+);
+CroogoRouter::connect('/users/topchart/:categoryname',
+	array(
+		'plugin' => 'users',
+		'controller' => 'users',
+		'action' => 'topchart'
+	),
+	array('pass' => array('categoryname'))
+);
+CroogoRouter::connect('/users/topchart/:categoryname/:online',
+	array(
+		'plugin' => 'users',
+		'controller' => 'users',
+		'action' => 'topchart'
+	),
+	array('pass' => array('categoryname','online'))
+);
 
-CroogoRouter::connect('/users/topchart/:categoryname', array(
-	'plugin' => 'users', 'controller' => 'users', 'action' => 'topchart'), array('pass' => array('categoryname')));
-CroogoRouter::connect('/users/topchart/:categoryname/:online', array(
-	'plugin' => 'users', 'controller' => 'users', 'action' => 'topchart'), array('pass' => array('categoryname','online')));	
-	
+/**
+ * User Messaging and Inviting
+ */
 CroogoRouter::connect('/users/messages/:username', array('plugin' => 'users', 'controller' => 'usermessage', 'action' => 'index'), array('pass' => array('username')));
-
 CroogoRouter::connect('/users/messages', array('plugin' => 'users', 'controller' => 'usermessage', 'action' => 'index'));
-
 CroogoRouter::connect('/users/joinuser/:id', array('plugin' => 'users', 'controller' => 'users', 'action' => 'joinuser'),array('pass'=>array('id')));
-
 CroogoRouter::connect('/users/invite', array('plugin' => 'users', 'controller' => 'invite', 'action' => 'index'));
 
+/**
+ * Billing and payments
+ */
+CroogoRouter::connect('/users/billing', array(
+	'plugin' => 'users',
+	'controller' => 'users',
+	'action' => 'billing')
+);
 CroogoRouter::connect('/users/paymentnotmade', array(
-	'plugin' => 'users', 'controller' => 'users', 'action' => 'paymentnotmade'));
-
+	'plugin' => 'users',
+	'controller' => 'users',
+	'action' => 'paymentnotmade')
+);
 CroogoRouter::connect('/users/claimoffer', array(
-	'plugin' => 'users', 'controller' => 'users', 'action' => 'claimoffer'));
-
+	'plugin' => 'users',
+	'controller' => 'users',
+	'action' => 'claimoffer')
+);
 CroogoRouter::connect('/users/paymentsetting', array(
-	'plugin' => 'users', 'controller' => 'users', 'action' => 'paymentsetting'));
+	'plugin' => 'users',
+	'controller' => 'users',
+	'action' => 'paymentsetting')
+);
--- www/app/Plugin/Users/View/Users/paymentnotmade.ctp
+++ www/app/Plugin/Users/View/Users/paymentnotmade.ctp
@@ -1,46 +1,33 @@
- 
 <?php
-echo $this->element("breadcrame",array('breadcrumbs'=>
+
+echo $this->element("breadcrame", array('breadcrumbs'=>
 	array('My Dashboard'=>'My Dashboard'))
-	);
- 
-	?>
+);
+
+?>
 
 <!--Wrapper main-content Block Start Here-->
 <div id="main-content">
   <div class="container">
     <div class="row-fluid">
       <div class="span12">
-        
       </div>
     </div>
     <div class="row-fluid">
-    <?php echo $this->Element("myaccountleft") ?>
+      <?php echo $this->Element("myaccountleft") ?>
       <div class="span9">
-      <h2 class="page-title">Payment Pending</h2>
-      <div class="StaticPageRight-Block">
-      <div class="PageLeft-Block">
-        
-         
+        <h2 class="page-title">Payment Pending</h2>
+        <div class="StaticPageRight-Block">
+          <div class="PageLeft-Block">
             <div class="control-group">
-			Your older payment is pending. <a href="<?php echo $this->webroot?>users/billing">Click Here</a> to make payment.
-              
+			           Your older payment is pending. <a href="<?php echo $this->webroot?>users/billing">Click Here</a> to make payment.
             </div>
-           
-           
-       </div>
-       
-  
-      </div>
+          </div>
+        </div>
       </div>
     </div>
-    <!-- @end .row --> 
-    
- 
-    
-    
-    
+    <!-- @end .row -->
   </div>
-  <!-- @end .container --> 
+  <!-- @end .container -->
 </div>
-<!--Wrapper main-content Block End Here--> 
+<!--Wrapper main-content Block End Here-->
