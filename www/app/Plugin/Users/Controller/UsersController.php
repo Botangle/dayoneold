bbf9f813ff13bb4b9842ab1ea29d4d3d102a54f7
David Baker <dbaker@acorncomputersolutions.com>
2014-04-11 18:16:02 -0600

    Cleaned out our model setup, it didn't work :-/
    And caused bugs instead

--- www/app/Plugin/Users/Controller/UsersController.php
+++ www/app/Plugin/Users/Controller/UsersController.php
@@ -1475,24 +1475,26 @@ debug($log);
         if (!empty($this->request->data)) {
 
             $this->Lesson->create();
-            $this->Lesson->visitor_id = $this->Auth->user('id');
 
             // @TODO: ideally we'd validate things before we start trying to mess with stuff here ...
 
-            if($this->Lesson->add($this->request->data)) {
+            $user_id_to_message = null;
+            $id = null;
+            $need_stripe_account_setup = false;
+            if($this->addLesson($this->request->data, $user_id_to_message, $id, $need_stripe_account_setup)) {
 
                 // if we need billing info, then we'll need to put our lesson message and session id generation
                 // on hold and work on the billing stuff instead
-                if($this->Lesson->need_stripe_account_setup) {
+                if($need_stripe_account_setup) {
                     $this->Session->write('initial_lesson_setup', true);
-                    $this->Session->write('new_lesson_user_id_to_message', $this->Lesson->user_id_to_message);
-                    $this->Session->write('new_lesson_lesson_id', $this->Lesson->id);
+                    $this->Session->write('new_lesson_user_id_to_message', $user_id_to_message);
+                    $this->Session->write('new_lesson_lesson_id', $id);
 
                     // redirect to our billing page to get setup with Stripe
                     $this->redirect(array('action' => 'billing'));
                 } else {
                     // otherwise, let's handle the post lesson add setup
-                    $this->postLessonAddSetup($this->Lesson->lesson_id, $this->Lesson->user_id_to_message);
+                    $this->postLessonAddSetup($id, $user_id_to_message);
                 }
 
             } else {
@@ -1509,6 +1511,84 @@ debug($log);
             $this->render('lessoncreate');
         }
 
+    }
+
+    /**
+     * Add a lesson for us
+     * Oh, it's ugly.  Is there anyway we can improve this over time and move it to a model that handles this stuff?
+     *
+     * @param $data
+     * @param $user_id_to_message
+     * @param $id
+     * @param $need_stripe_account_setup
+     * @return bool
+     */
+    private function addLesson($data, &$user_id_to_message, &$id, &$need_stripe_account_setup)
+    {
+        // this gets run when a student proposes a lesson to a tutor
+        if (isset($data['Lesson']['tutor']) && $data['Lesson']['tutor'] != "") {
+
+            $user_id_to_message = (int)$data['Lesson']['tutor'];
+
+            $data['Lesson']['tutor'] = $user_id_to_message;
+            $data['Lesson']['created'] = $this->Auth->user('id');
+            $data['Lesson']['add_date'] = date('Y-m-d');
+            $data['Lesson']['readlesson'] = '1';
+            $data['Lesson']['readlessontutor'] = '0';
+            $data['Lesson']['is_confirmed'] = '0';
+            $data['Lesson']['laststatus_tutor'] = 0;
+            $data['Lesson']['laststatus_student'] = 1;
+
+            // @TODO: decide whether we want to track whether this lesson's user has been vetted as a billable Customer here or not
+            // seems like it might be better to check that right against the Users table with a join ...
+
+            $student = $this->User->find('first', array('conditions' => array('User.id' => $this->Auth->user('id'))));
+
+            // if we don't have a stripe customer id for this student, then we need billing info
+            $need_stripe_account_setup = (!isset($student['User']['stripe_customer_id'])
+                || $student['User']['stripe_customer_id'] == "")
+                ? true
+                : false;
+
+        } else {
+            // this gets run when a tutor creates a lesson to do with a student on the /users/createlessons page
+            $user = ClassRegistry::init(array('class' => 'Users.User', 'alias' => 'User'));
+            $tutorid = $user->find('first', array('conditions' => array('username' => $data['Lesson']['tutorname'])));
+            $tutorid = $tutorid['User']['id'];
+
+            // we'll want to message this person below
+            $user_id_to_message = (int)$tutorid;
+
+            $data['Lesson']['tutor'] = $this->Auth->user('id');
+            $data['Lesson']['created'] = $tutorid;
+            $data['Lesson']['add_date'] = date('Y-m-d');
+            $data['Lesson']['readlesson'] = '0';
+            $data['Lesson']['readlessontutor'] = '1';
+            $data['Lesson']['is_confirmed'] = '0';
+            $data['Lesson']['laststatus_tutor'] = 1;
+            $data['Lesson']['laststatus_student'] = 0;
+        }
+
+        if ($this->User->save($data, false)) {
+            $id = $this->User->getLastInsertId();
+
+            // @TODO: check on these items, there may be bugs.  Looks like we're unsetting our lesson data before trying to save a lesson
+            // that probably won't work.  Or rather, it may generate duplicate lessons with no data or an error?
+            if (isset($data['Lesson']['parent_id']) && $data['Lesson']['parent_id'] != "") {
+                unset($data['Lesson']);
+                $data['Lesson']['parent_id'] = $id;
+                $this->User->save($data);
+            }
+            if (!isset($data['Lesson']['parent_id'])) {
+                unset($data['Lesson']);
+                $data['Lesson']['parent_id'] = $id;
+                $this->User->save($data);
+            }
+
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
