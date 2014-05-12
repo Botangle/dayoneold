32bf9847e2a86f7055737097432684ba1143d12a
Zharfan Mazli <zhaff@neptunescripts.com>
2014-05-12 23:33:23 +0800

    Fixed news details page (image and news details record).

--- www/app/Plugin/News/Controller/NewsController.php
+++ www/app/Plugin/News/Controller/NewsController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 App::uses('NewsAppController', 'News.Controller');
 App::uses('Croogo', 'Lib');
 
@@ -47,14 +47,15 @@ class NewsController extends NewsAppController {
  * @access public
  */
 	public $uses = array('News.News');
-	function beforeFilter(){
-	 
+
+	function beforeFilter() {
+
 		parent::beforeFilter();
-		 $this->Security->validatePost = false;
-		 $this->Security->csrfCheck = false;
-		 
-		 $this->Security->unlockedActions = array('index');
-		 $this->Auth->allow('index','detail'); 
+		$this->Security->validatePost = false;
+		$this->Security->csrfCheck = false;
+
+		$this->Security->unlockedActions = array('index');
+		$this->Auth->allow('index', 'detail');
 	}
 
 /**
@@ -64,15 +65,14 @@ class NewsController extends NewsAppController {
  * @access public
  * $searchField : Identify fields for search
  */
-	public function admin_index() {  
-			$this->set('title_for_layout', __d('croogo', 'News'));
-			$this->News->recursive = 0;		 
-			/*$this->paginate['New']['conditions'] = ' `New`.`parent_id` IS NULL';*/
-			$this->paginate['News']['order'] = 'News.date Desc'; 
-			$this->set('news', $this->paginate());   
-			
-			$this->set('displayFields', $this->News->displayFields());
-		  
+	public function admin_index() {
+		$this->set('title_for_layout', __d('croogo', 'News'));
+		$this->News->recursive = 0;
+		/* $this->paginate['New']['conditions'] = ' `New`.`parent_id` IS NULL'; */
+		$this->paginate['News']['order'] = 'News.date Desc';
+		$this->set('news', $this->paginate());
+
+		$this->set('displayFields', $this->News->displayFields());
 	}
 
 /**
@@ -84,34 +84,32 @@ class NewsController extends NewsAppController {
 	public function admin_add() {
 		if (!empty($this->request->data)) {
 			$this->News->create();
-			 
-			 		 
- $filename = "";
-if (!empty($this->request->data['News']['image']['tmp_name']) && is_uploaded_file($this->request->data['News']['image']['tmp_name'])) {
-				 
-	$filename = str_replace(" ","_",basename($this->request->data['News']['image']['name']));
-	 $dir = 	WWW_ROOT .'uploads' . DS . 'news' ; 
-	 
-				if(!is_dir($dir)){ 
-					mkdir($dir,0777); 
-				} 
-				move_uploaded_file(
-					$this->data['News']['image']['tmp_name'],
-					$dir. DS . $filename
-				); 
-			}
-			 
-			 if(isset( $this->request->data['News']['id']) &&  $this->request->data['News']['id']!=""){
-			  $dir = 	WWW_ROOT .'uploads' . DS . 'news'.DS ; 
-				if(file_exists($dir.$this->request->data['image_old']) && $this->request->data['image_old']!=""){
-					unlink($dir.$this->request->data['image_old']);
+
+
+			$filename = "";
+			if (!empty($this->request->data['News']['image']['tmp_name']) && is_uploaded_file($this->request->data['News']['image']['tmp_name'])) {
+
+				$filename = str_replace(" ", "_", basename($this->request->data['News']['image']['name']));
+				$dir = WWW_ROOT . 'uploads' . DS . 'news';
+
+				if (!is_dir($dir)) {
+					mkdir($dir, 0777);
 				}
-				if($filename==""){
+				move_uploaded_file(
+						$this->data['News']['image']['tmp_name'], $dir . DS . $filename
+				);
+			}
+
+			if (isset($this->request->data['News']['id']) && $this->request->data['News']['id'] != "") {
+				$dir = WWW_ROOT . 'uploads' . DS . 'news' . DS;
+				if (file_exists($dir . $this->request->data['image_old']) && $this->request->data['image_old'] != "") {
+					unlink($dir . $this->request->data['image_old']);
+				}
+				if ($filename == "") {
 					$filename = $this->request->data['image_old'];
 				}
-				 
-			 }
-			 $this->request->data['News']['image'] = $filename;
+			}
+			$this->request->data['News']['image'] = $filename;
 			if ($this->News->save($this->request->data)) {
 				$this->Session->setFlash(__d('croogo', 'The News has been saved'), 'default', array('class' => 'success'));
 				$this->redirect(array('action' => 'index'));
@@ -119,7 +117,6 @@ if (!empty($this->request->data['News']['image']['tmp_name']) && is_uploaded_fil
 				$this->Session->setFlash(__d('croogo', 'The News could not be saved. Please, try again.'), 'default', array('class' => 'error'));
 			}
 		}
-		 
 	}
 
 /**
@@ -129,32 +126,32 @@ if (!empty($this->request->data['News']['image']['tmp_name']) && is_uploaded_fil
  * @return void
  * @access public
  */
-	public function admin_edit($id = null) {  
+	public function admin_edit($id = null) {
 		if (!empty($this->request->data)) {
-		 
-if (!empty($this->request->data['News']['image']['tmp_name']) && is_uploaded_file($this->request->data['News']['image']['tmp_name'])) {
-				 
-	$filename = str_replace(" ","_",basename($this->request->data['News']['image']['name']));
-	 $dir = 	WWW_ROOT .'uploads' . DS . 'news' ; 
-	 
-				if(!is_dir($dir)){ 
-					mkdir($dir,0777); 
+
+			if (!empty($this->request->data['News']['image']['tmp_name']) && is_uploaded_file($this->request->data['News']['image']['tmp_name'])) {
+
+				$filename = str_replace(" ", "_", basename($this->request->data['News']['image']['name']));
+				$dir = WWW_ROOT . 'uploads' . DS . 'news';
+
+				if (!is_dir($dir)) {
+					mkdir($dir, 0777);
 				}
-				 echo $dir.$this->data['News']['old_image']; die;
+				echo $dir . $this->data['News']['old_image'];
+				die;
 				move_uploaded_file(
-					$this->data['News']['image']['tmp_name'],
-					$dir. DS . $filename
+						$this->data['News']['image']['tmp_name'], $dir . DS . $filename
 				);
-				if(file_exists($dir.$this->data['News']['old_image'])){
-					unlink($dir.$this->data['News']['old_image']);
+				if (file_exists($dir . $this->data['News']['old_image'])) {
+					unlink($dir . $this->data['News']['old_image']);
 				}
-				 
-			}else{
+			} else {
 				$filename = $this->data['News']['old_image'];
 			}
-			pr($this->request->data); die;
-			 
-			 $this->request->data['News']['image'] = $filename;
+			pr($this->request->data);
+			die;
+
+			$this->request->data['News']['image'] = $filename;
 			if ($this->News->save($this->request->data)) {
 				$this->Session->setFlash(__d('croogo', 'The News has been saved'), 'default', array('class' => 'success'));
 				$this->redirect(array('action' => 'index'));
@@ -164,10 +161,9 @@ if (!empty($this->request->data['News']['image']['tmp_name']) && is_uploaded_fil
 		} else {
 			$this->request->data = $this->News->read(null, $id);
 		}
-		 
+
 		$this->set('editFields', $this->News->editFields());
 	}
- 
 
 /**
  * Admin delete
@@ -186,30 +182,30 @@ if (!empty($this->request->data['News']['image']['tmp_name']) && is_uploaded_fil
 			$this->redirect(array('action' => 'index'));
 		}
 	}
-	public function index(){ 
-			$this->News->recursive = 0;	
-			 $name = "";
-			 $cond= array('status'=>"1");
-			if(!empty($this->request->data)){
-			 
-				 $name = $this->request->data['id'];
-				$cond= array('status'=>"1",'id'=>$name);
-			} 
-			$news = $this->News->find('all',array('conditions'=> $cond));
-			$this->set(compact('news'));
+
+	public function index() {
+		$this->News->recursive = 0;
+		$name = "";
+		$cond = array('status' => "1");
+		if (!empty($this->request->data)) {
+
+			$name = $this->request->data['id'];
+			$cond = array('status' => "1", 'id' => $name);
+		}
+		$news = $this->News->find('all', array('conditions' => $cond));
+		$this->set(compact('news'));
 	}
-	public function detail(){ 
-			$this->News->recursive = 0;	
-			 $name = "";
-			 $cond= array('status'=>"1");
-			if(!empty($this->request->data)){
-			 
-				 $name = $this->request->data['id'];
-				$cond= array('status'=>"1",'id'=>$name);
-			} 
-			$news = $this->News->find('first',array('conditions'=> $cond));
-			$this->set(compact('news'));
+
+	public function detail() {
+		$this->News->recursive = 0;
+
+		$news = $this->News->find('first', array(
+			'conditions' => array(
+				'News.status' => '1',
+				'News.id' => $this->request->params['id'],
+			),
+		));
+		$this->set(compact('news'));
 	}
-   
 
 }
