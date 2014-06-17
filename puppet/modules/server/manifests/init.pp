0146ae70508126093cc0ec63e08bd85d13c8181e
David Baker <dbaker@acorncomputersolutions.com>
2014-06-17 14:43:03 -0600

    Removed unneeded dotdeb package repo that was causing issues

--- puppet/modules/server/manifests/init.pp
+++ puppet/modules/server/manifests/init.pp
@@ -1,17 +1,6 @@
 class server ( $hostname ) {
 
-    file { "/etc/apt/sources.list.d/dotdeb.list":
-        ensure => file,
-        path   => "/etc/apt/sources.list.d/dotdeb.list",
-        source => "puppet:///modules/server/dotdeb.list",
-        owner  => "root",
-        group  => "root",
-    }
-    -> exec { "import dotdeb gpg":
-        onlyif  => "test `/usr/bin/apt-key list | /bin/grep dotdeb.org | /usr/bin/wc -l` -eq 0",
-		command => "/usr/bin/wget -q http://www.dotdeb.org/dotdeb.gpg -O -| /usr/bin/apt-key add -",
-	}
-    -> exec { "update":
+    exec { "update":
         path    => "/bin:/usr/bin",
         command => "apt-get update",
     }
