60f86eef0d259048641e7f58efda8c3329f68db4
David Baker <dbaker@acorncomputersolutions.com>
2014-07-07 13:12:39 -0600

    Added in the bottom section of our main layout

--- laravel/app/views/layout.blade.php
+++ laravel/app/views/layout.blade.php
@@ -55,9 +55,55 @@
 @include('partials.footer-middle')
 <!--Wrapper main-content1 Block End Here-->
 
-{{-- echo $this->element('footerbottom'); ?>
-//echo $this->Blocks->get('scriptBottom');
-//echo $this->Js->writeBuffer();
---}}
+<div id="footer">
+    <div class="container">
+        <div class="row-fluid">
+            <div class="span4 fotter-left"> &copy; {{ date('Y'); }}. All right reserved. botangle.com </div>
+            <div class="span5 fotter-right pull-right">
+                <ul class="nav nav-pills pull-right">
+                    {{--
+                    <li><a href="#" title="Blog">Blog</a></li>
+                    <li><a href="#" title="Sitemap">Sitemap</a></li>
+                    <li>
+                    echo $this->Html->link(
+                        __('Terms of use'), '/terms'
+                        , array('class' => ' active', 'title' => __('Terms of use'))
+                    );
+					</li>
+					<li>
+                    echo $this->Html->link(
+                        __('Privacy Policy'), '/privacy'
+                        , array('class' => ' active', 'title' => __('Privacy Policy'))
+                    );
+					</li>
+                    --}}
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<link rel="stylesheet" type="text/css" href="/css/prettyCheckable.css" />
+<script type="text/javascript" src="/js/prettyCheckable.js"></script>
+
+<script type='text/javascript'>
+    $().ready(function() {
+
+        if( $('input:checkbox').length )  {
+            $('input:checkbox').prettyCheckable({
+                color: 'red'
+
+            });
+        }
+    });
+
+    <?php /* Lucky Orange JS tracking script */ ?>
+    window.__wtw_lucky_site_id = 23539;
+    (function() {
+        var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
+        wa.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://cdn') + '.luckyorange.com/w.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
+    })();
+</script>
 </body>
 </html>
