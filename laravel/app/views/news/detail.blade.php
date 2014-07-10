c90f65dc52e8057bbce18eaacb846f92b8b7e8c7
Zharfan Mazli <zhaff@neptunescripts.com>
2014-07-10 15:48:13 +0800

    Formated news date. Enable default news image.

--- laravel/app/views/news/detail.blade.php
+++ laravel/app/views/news/detail.blade.php
@@ -18,17 +18,18 @@
 					<div class="PageLeft-Block">
 						<div class="row-fluid">
 							<div class="span3 media-img">
+								<?php //TODO: News image upload ?>
 								<?php //if (file_exists(WWW_ROOT . DS . 'uploads' . DS . 'news' . DS . $news['News']['image']) && $news['News']['image'] != "") { ?>
 									<!--<img src="<?php //echo $this->webroot . 'uploads/news/' . $news['News']['image'] ?> ">-->
 								<?php //} else { ?>
-									<!--<img src="/images/media-1.jpg" alt="media">-->
+									<img src="/images/media-1.jpg" alt="media">
 								<?php //} ?>
 							</div>
 							<div class="span9 media-text">
 								<p class="FontStyle20"><a href="#" >{{{ $news->title }}}</a></p>
 								<p>{{{ $news->details }}}</p>
 								<br>
-								<p>Posted on: {{{ $news->date }}} </p></div>
+								<p>Posted on: {{ date("M d,Y",strtotime($news->date)) }} </p></div>
 						</div> </div>
 
 				</div>
