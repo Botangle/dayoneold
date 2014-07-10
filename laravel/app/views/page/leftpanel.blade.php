94c7236eac8fb23e82528b509102f2af2388ddca
Zharfan Mazli <zhaff@neptunescripts.com>
2014-07-11 01:54:30 +0800

    Changed menu link to use the html macro.

--- laravel/app/views/page/leftpanel.blade.php
+++ laravel/app/views/page/leftpanel.blade.php
@@ -1,13 +1,7 @@
 <div class="span3 LeftMenu-Block">
     <ul>
-        <li>
-            {{ link_to_action('PageController@getReportbug', trans('About Us'), [], ['class' => '', 'title' => trans('About Us')]) }}
-        </li>
-        <li>
-            {{ link_to_action('PageController@getReportbug', trans('Contact Us'), [], ['class' => '', 'title' => trans('Contact Us')]) }}
-        </li>
-        <li>
-            {{ link_to_action('PageController@getReportbug', trans('Report Bug'), [], ['class' => '', 'title' => trans('Report Bug')]) }}
-        </li>
+		{{ HTML::leftmenu_link('about', trans('About Us')) }}
+		{{ HTML::leftmenu_link('contact', trans('Contact Us')) }}
+		{{ HTML::leftmenu_link('reportbug', trans('Report Bug')) }}
     </ul>
 </div>
