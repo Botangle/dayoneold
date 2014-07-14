12a3e2a87dca2e4755d6c2ab79cb9033d2d0ac73
Zharfan Mazli <zhaff@neptunescripts.com>
2014-07-14 10:38:27 +0800

    Changed input names.

--- laravel/app/views/page/reportbug.blade.php
+++ laravel/app/views/page/reportbug.blade.php
@@ -11,22 +11,22 @@
 
 			<div class="form-group span6 ">
 				<label class="sr-only" for="your_name">{{{ trans('Your Name') }}}</label>
-				<input type="text" class="form-control textbox1 " name="data[page][name]" id="your_name" placeholder="{{{ trans('Your Name') }}}" required="required">
+				<input type="text" class="form-control textbox1 " name="name" id="your_name" placeholder="{{{ trans('Your Name') }}}" required="required">
 			</div>
 			<div class="form-group span6">
 				<label class="sr-only" for="email">{{{ trans('Your Email Address') }}}</label>
-				<input type="email" class="form-control textbox1" id="email" placeholder="{{{ trans('Your Email Address') }}}" name="data[page][email]" required="required">
+				<input type="email" class="form-control textbox1" id="email" placeholder="{{{ trans('Your Email Address') }}}" name="email" required="required">
 			</div>
 		</div>
 		<div class="row-fluid marT10">
 			<div class="span12 form-group">
 				<label class="sr-only" for="category">{{{ trans('Subject') }}}</label>
-				<input type="text" class="form-control textbox1" id="category" placeholder="{{{ trans('Your Subject') }}}" required="required"  name="data[page][subject]">
+				<input type="text" class="form-control textbox1" id="category" placeholder="{{{ trans('Your Subject') }}}" required="required"  name="subject">
 			</div></div>
 		<div class="row-fluid">
 			<div class="span12 form-group marT10">
 				<label class="sr-only" for="message">{{{ trans('Error') }}}</label>
-				<textarea id="select-subject" class="textarea" placeholder="{{{ trans('Your Message') }}}" rows="3" required="required"  name="data[page][error]"></textarea>
+				<textarea id="select-subject" class="textarea" placeholder="{{{ trans('Your Message') }}}" rows="3" required="required"  name="message"></textarea>
 			</div></div>
 		<div class="row-fluid marT10">
 			<div class="span12 ">
