c9d106f455d5a46b3a3bae58ddc2165d50c186ab
Zharfan Mazli <zhaff@neptunescripts.com>
2014-07-14 17:42:00 +0800

    Formating codes.

--- laravel/app/views/page/home.blade.php
+++ laravel/app/views/page/home.blade.php
@@ -11,85 +11,87 @@ echo $this->Html->css(array(
 ));
 --}}
 <script>
-    $(function() {
-        $.getJSON( "/subject/search",function(response){
-            data = response;
+	$(function() {
+		$.getJSON("/subject/search", function(response) {
+			data = response;
 
-            $( "#searchvalue" ).autocomplete({
-                minLength: 0,
-                source: data,
-                focus: function( event, ui ) {
-                    $( "#searchvalue" ).val( ui.item.label );
-                    return false;
-                },
-                select: function( event, ui ) { console.log(ui)
-                    $( "#searchvalue" ).val( ui.item.label );
+			$("#searchvalue").autocomplete({
+				minLength: 0,
+				source: data,
+				focus: function(event, ui) {
+					$("#searchvalue").val(ui.item.label);
+					return false;
+				},
+				select: function(event, ui) {
+					console.log(ui)
+					$("#searchvalue").val(ui.item.label);
 
 
-                    return false;
-                }
-            })
-                .data( "ui-autocomplete" )._renderItem = function( ul, item ) {
-                return $( "<li>" )
-                    .append( "<a>" + item.label + "</a>" )
-                    .appendTo( ul );
-            };
-        })
-    });
+					return false;
+				}
+			})
+					.data("ui-autocomplete")._renderItem = function(ul, item) {
+				return $("<li>")
+						.append("<a>" + item.label + "</a>")
+						.appendTo(ul);
+			};
+		})
+	});
 
-    jQuery(function() {
+	jQuery(function() {
 
-        function split( val ) {
-            return val.split( /,\s*/ );
-        }
-        function extractLast( term ) {
-            return split( term ).pop();
-        }
-        var typeid = "";
-        jQuery( "#searchvalue2" )
-            // don't navigate away from the field on tab when selecting an item
-            .bind( "keydown", function( event ) { typeid = this.id;
-                if ( event.keyCode === jQuery.ui.keyCode.TAB &&
-                    $( this ).data( "ui-autocomplete" ).menu.active ) {
-                    event.preventDefault();
-                }
-            })
-            .autocomplete({
-                source: function( request, response ) {
+		function split(val) {
+			return val.split(/,\s*/);
+		}
+		function extractLast(term) {
+			return split(term).pop();
+		}
+		var typeid = "";
+		jQuery("#searchvalue2")
+				// don't navigate away from the field on tab when selecting an item
+				.bind("keydown", function(event) {
+					typeid = this.id;
+					if (event.keyCode === jQuery.ui.keyCode.TAB &&
+							$(this).data("ui-autocomplete").menu.active) {
+						event.preventDefault();
+					}
+				})
+				.autocomplete({
+					source: function(request, response) {
 
-                    var url = "/subject/search";
+						var url = "/subject/search";
 
-                    $.getJSON( url, {
-                        term: extractLast( request.term )
-                    }, response );
-                },
-                search: function() {
-                    // custom minLength
-                    var term = extractLast( this.value );
-                    if ( term.length < 2 ) {
-                        return false;
-                    }
-                },
-                focus: function() {
-                    // prevent value inserted on focus
-                    return false;
-                },
-                select: function( event, ui ) {
-                    var terms = split( this.value );
-                    if(typeid=='LessonTutor'){
-                        jQuery("#"+typeid+"Value").val(ui.item.id)
-                    }
-                    // remove the current input
-                    terms.pop();
-                    // add the selected item
-                    terms.push( ui.item.value );
-                    // add placeholder to get the comma-and-space at the end
-                    terms.push( "" );
-                    this.value = terms.join( " " );
-                    return false;
-                }
-            });
-    });
+						$.getJSON(url, {
+							term: extractLast(request.term)
+						}, response);
+					},
+					search: function() {
+						// custom minLength
+						var term = extractLast(this.value);
+						if (term.length < 2) {
+							return false;
+						}
+					},
+					focus: function() {
+						// prevent value inserted on focus
+						return false;
+					},
+					select: function(event, ui) {
+						var terms = split(this.value);
+						if (typeid == 'LessonTutor') {
+							jQuery("#" + typeid + "Value").val(ui.item.id)
+						}
+						// remove the current input
+						terms.pop();
+						// add the selected item
+						terms.push(ui.item.value);
+						// add placeholder to get the comma-and-space at the end
+						terms.push("");
+						this.value = terms.join(" ");
+						return false;
+					}
+				});
+	});
 </script>
 
 
@@ -177,52 +179,52 @@ echo $this->Html->css(array(
     </center>
 </div>
 <div class="row-fluid">
-        @foreach ($featuredUsers as $user)
-            <div class="span4 Tutor-list1">
-                <div class="tutor-img">
-                    <a href="{{{ action('UserController@getView', ['username' => $user->username]) }}}">
-                        <img src="{{ $user->picture or '/images/tutor.jpg' }}" class="img-circle" alt="student" style="width: 195px; height: 195px">
-                    </a>
-                </div>
-				<div class="tutor-title">
-					<h3>
-						<a href="{{{ action('UserController@getView', ['username' => $user->username]) }}}">{{{ $user->full_name }}}</a>
-					</h3>
-					<p>{{{ $user->qualification }}}</p>
-				 </div>
-				<div class="tutor-bio">
-                <p>{{{ $user->extracurricular_interests }}}</p>
-                <div class="social">
+	@foreach ($featuredUsers as $user)
+	<div class="span4 Tutor-list1">
+		<div class="tutor-img">
+			<a href="{{{ action('UserController@getView', ['username' => $user->username]) }}}">
+				<img src="{{ $user->picture or '/images/tutor.jpg' }}" class="img-circle" alt="student" style="width: 195px; height: 195px">
+			</a>
+		</div>
+		<div class="tutor-title">
+			<h3>
+				<a href="{{{ action('UserController@getView', ['username' => $user->username]) }}}">{{{ $user->full_name }}}</a>
+			</h3>
+			<p>{{{ $user->qualification }}}</p>
+		</div>
+		<div class="tutor-bio">
+			<p>{{{ $user->extracurricular_interests }}}</p>
+			<div class="social">
                 {{-- social link - FB --}}
                 @if (!empty($user->link_fb))
-                    <a href="{{{ $user->link_fb }}}" class="img-circle-left">
-                        <img src="/img/facebook.png">
-                    </a>
+				<a href="{{{ $user->link_fb }}}" class="img-circle-left">
+					<img src="/img/facebook.png">
+				</a>
                 @endif
-				
+
 				{{-- social link - Twitter --}}
                 @if (!empty($user->link_twitter))
-                    <a href="{{{ $user->link_twitter }}}" class="img-circle-left">
-                        <img src="/img/twitter.png">
-                    </a>
+				<a href="{{{ $user->link_twitter }}}" class="img-circle-left">
+					<img src="/img/twitter.png">
+				</a>
                 @endif
-				
+
 				{{-- social link - Google+ --}}
                 @if (!empty($user->link_googleplus))
-                    <a href="{{{ $user->link_googleplus }}}" class="img-circle-left">
-                        <img src="/img/google.png">
-                    </a>
+				<a href="{{{ $user->link_googleplus }}}" class="img-circle-left">
+					<img src="/img/google.png">
+				</a>
                 @endif
-				
+
 				{{-- social link - Thumblr --}}
                 @if (!empty($user->link_thumblr))
-                    <a href="{{{ $user->link_thumblr }}}" class="img-circle-left">
-                        <img src="/img/trumbler.png">
-                    </a>
+				<a href="{{{ $user->link_thumblr }}}" class="img-circle-left">
+					<img src="/img/trumbler.png">
+				</a>
                 @endif
-        </div>
-    </div>
-            </div>
+			</div>
+		</div>
+	</div>
     @endforeach
 </div>
 @endif
