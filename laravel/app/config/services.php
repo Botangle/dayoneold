5459cbca5649bdaba13b1c320de12124167de547
Martyn Ling <mling@orthomeo.com>
2014-09-03 09:14:35 +0100

    Update .gitignore to exclude local/services.php file that contains secure keys.

--- laravel/app/config/services.php
+++ laravel/app/config/services.php
@@ -12,6 +12,9 @@ return array(
 	| default location for this type of information, allowing packages
 	| to have a conventional place to find your various credentials.
 	|
+	| Keys, tokens, secrets, etc. should not be committed to the repo but should be
+	| held in an appropriate location where they're accessible only to those who
+	| need to know them.
 	*/
 
 	'mailgun' => array(
@@ -29,8 +32,8 @@ return array(
 	),
 
     's3' => array(
-        'accessKey' => '',
-        'secretKey' => '',
+        'accessKey'         => '', // Set according to environment e.g. in local/services.php (not in repo)
+        'secretKey'         => '', // Set according to environment e.g. in local/services.php (not in repo)
         'bucket'            => 'botangleassets',
         'region'            => Aws\Common\Enum\Region::US_EAST_1,
         'url'               => "https://s3.amazonaws.com",
