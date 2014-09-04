3cb666674563a873cdbbb2b7f0edbfd6275add2f
Martyn Ling <mling@orthomeo.com>
2014-09-04 11:56:24 +0100

    Switched to using the latest version of OpenTok.

---
+++ laravel/app/classes/Twiddla/Twiddla.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * TwiddlaComponent.php
+ *
+ * @author: David Baker <dbaker@acorncomputersolutions.com
+ * Date: 3/27/14
+ * Time: 6:55 PM
+ */
+
+App::uses('Component', 'Controller');
+class TwiddlaComponent extends Component {
+
+    /**
+     * Twiddla username
+     * @var string
+     */
+    public $username;
+
+    /**
+     * Twiddla password
+     * @var string
+     */
+    public $password;
+
+    /**
+     * @return mixed
+     */
+    public function getMeetingId()
+    {
+        // @TODO: shift to using SSL for this long-term?
+        // @TODO: do we want to do any type of error checking in here?  currently, this is pretty bare bones ... :-/
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://www.twiddla.com/API/CreateMeeting.aspx");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,
+            "username={$this->username}&password={$this->password}&controltype=1");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec($ch);
+        curl_close($ch);
+        return $server_output;
+    }
+} 
---
+++ laravel/app/eventHandlers/LessonHandler.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: martyn ling <mling@str8-4ward.com>
+ * Date: 04/09/14
+ * Time: 10:12
+ */
+
+namespace Botangle\eventHandlers;
+
+
+class LessonHandler {
+
+} 
