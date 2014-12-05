63e1fd1394d22a83ad7603ba90b47c4810f8a565
Martyn Ling <mling@orthomeo.com>
2014-12-05 12:16:43 +0000

    Braintree debugging

--- laravel/app/models/Transaction.php
+++ laravel/app/models/Transaction.php
@@ -360,10 +360,11 @@ class Transaction extends MagniloquentContextsPlus {
         // sandbox or production
         Braintree_Configuration::environment(Config::get('services.braintree.mode'));
         Log::info("Braintree Mode: ". Config::get('services.braintree.mode'));
-        
+
         // set other items as well
         Braintree_Configuration::merchantId(Config::get('services.braintree.merchantId'));
         Braintree_Configuration::publicKey(Config::get('services.braintree.publicKey'));
+        Log::info("Braintree MerchantId: ". Config::get('services.braintree.merchantId'));
 
         // set the Braintree private key
         $key = Config::get('services.braintree.privateKey');
