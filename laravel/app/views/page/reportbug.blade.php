1fe03bd38577c8775c02923ebc0106acec97e693
David Baker <dbaker@acorncomputersolutions.com>
2015-02-17 10:43:07 -0700

    Revert "Revert "Merge branch 'hotfix-adding-honeypot-form-validations'""
    Putting our honeypot stuff back in
    
    This reverts commit 3b295ed17119ee8083840ff41d97ab18a5bbea74.

--- laravel/app/views/page/reportbug.blade.php
+++ laravel/app/views/page/reportbug.blade.php
@@ -7,6 +7,8 @@
 @section('content')
 <div class="PageLeft-Block">
 	<form class="form-inline form-horizontal" role="form" action="/reportbug"  method="post">
+	    {{ Form::honeypot('super_important', 'super_important_time') }}
+
 		<div class="row-fluid">
 
 			<div class="form-group span6 ">
